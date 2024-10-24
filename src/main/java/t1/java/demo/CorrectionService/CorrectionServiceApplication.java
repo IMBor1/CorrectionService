53f873ecddee3c5f104438b9f6f2f322b402b2e0
IMBor1 <ilacerber1@gmail.com>
2024-10-24 23:25:34 +0300

    init

---
+++ src/main/java/t1/java/demo/CorrectionService/CorrectionServiceApplication.java
@@ -0,0 +1,13 @@
+package t1.java.demo.CorrectionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrectionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/t1/java/demo/CorrectionService/CorrectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package t1.java.demo.CorrectionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
